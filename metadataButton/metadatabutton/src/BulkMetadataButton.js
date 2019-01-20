3285968a82c7baa2d554fe92d9ed809b84acd98d
jsabbath <jsabbath@asu.edu>
2019-01-19 20:30:05 -0700

    Added Grid/Color Change

--- metadataButton/metadatabutton/src/BulkMetadataButton.js
+++ metadataButton/metadatabutton/src/BulkMetadataButton.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import {withStyles} from '@material-ui/core/styles';
 import {creatMuiTheme} from '@material-ui/core/styles';
 import './style.css';
+import Grid from '@material-ui/core/Grid';
  
 const style = theme => ({
     root: {
@@ -20,15 +22,31 @@ const style = theme => ({
 });
 
 class BulkMetadataButton extends React.Component {	    
-		
+		constructor(props) {
+            super(props);
+            this.state = { contentFileName: "" };
+        }
 		render(){
 				return (
 					<div>
-								<h3>Stuff</h3>					
+								<h3>Metadata Spreadsheet Upload</h3>					
 							    <p>
 								
 								</p>
-								<div>
+								
+                                <Grid item xs = {8}>
+                                    <TextField
+                                        id="metadataFile"
+                                        label="Metadata File"
+                                        multiline
+                                        disabled
+                                        InputLabelProps={{
+                                            shrink: true,
+                                        }}
+                                       // error={this.state.fieldErrors.file ? true : false}
+                                        value={this.state.contentFileName}
+                                        margin="normal"
+                                    />
 									<input 
 									accept="*"
 									className={"hidden"}
@@ -38,13 +56,13 @@ class BulkMetadataButton extends React.Component {
 									//ref={input => {this.fileInput = input;}} 
 									/>
 	
-								</div>
 								<label htmlFor="metadata-upload-input">
-								<Button variant="raised" color="secondary" component="span">
+								<Button variant="contained" component="span">
 									Upload Metadata
 									
 								</Button>
 								</label>
+                                </Grid>
 					</div>
 			);
 		}
