d1bc61c32e35140c08f120876dae9588361ee8a0
jsabbath <jsabbath@asu.edu>
2019-02-13 23:28:11 -0700

    US124Task130 Research project's generic template use

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,13 +4,13 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     #ex: /polls/
-    url(r'^$', views.index, name='index'),    
+    url('', views.IndexVeiw.as_view(), name='index'),    
     #ex /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url('<int:pk>/', views,DetailView.as_view(), name='detail'),
     #ex /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url('<int:pk>/results/' views.ResultsView.as_view(), name='results'),
     #ex: /polls/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url('<int:question_id>/vote/', views.vote, name='vote'),
     
 ]
 
